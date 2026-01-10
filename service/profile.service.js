0798f23d1fe1ede310744a803886580bbdfa17e2
Amareshraja <amareshraja@centizen.com>
2026-01-10 17:45:57 +0530

    fix:  code changes

--- service/profile.service.js
+++ service/profile.service.js
@@ -18,6 +18,10 @@ const axios = require('axios');
 const fs = require('fs');
 const { S3Client } = require('@aws-sdk/client-s3');
 
+
+const Buffer = require('buffer');
+const PutObjectCommand = require('@aws-sdk/client-s3')
+
 const s3 = new S3Client({
   region: CONFIG.AWS_REGION,
   credentials: {
