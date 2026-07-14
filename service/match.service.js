80551714b96a3af7985b6789f95a6514471ddadd
Amareshraja <amareshraja@centizen.com>
2026-07-15 00:25:09 +0530

    fix :  dosham fix

--- service/match.service.js
+++ service/match.service.js
@@ -74,7 +74,7 @@ const getMatchingList = async (req) => {
         },
         {
           model: ZodiacDetails,
-          attributes: ['id'],
+          attributes: ['id', 'dosham', 'patham'],
           where: zodiacWhereCondition,
           include: [
             {
